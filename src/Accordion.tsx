5a5b49e5d9a1a7fa5cec169dc0d6508a9886a9af
fponce <fponce@cash-converters.es>
2023-09-13 14:13:14 +0200

    #1: Dummy Accordion Component

--- src/Accordion.tsx
+++ src/Accordion.tsx
@@ -1,32 +1,5 @@
 import "./Accordion.scss";
-import { useEffect, useState } from "react";
 
-export interface AccordionProps {
-	header: JSX.Element;
-	content: JSX.Element;
-	collapsed?: boolean;
-}
-
-export function Accordion({
-	header,
-	content,
-	collapsed = true,
-}: AccordionProps): JSX.Element {
-	const [collapsedStatus, setCollapsedStatus] = useState(collapsed);
-
-	useEffect(() => {
-		setCollapsedStatus(collapsed);
-	}, [collapsed]);
-
-	return (
-		<div className="accordion">
-			<div
-				className="accordion-header"
-				onClick={() => setCollapsedStatus(!collapsedStatus)}
-			>{header}</div>
-			<div
-				className={`accordion-content ${collapsedStatus ? "hidden" : ""}`}
-			>{content}</div>
-		</div>
-	);
+export function Accordion(): JSX.Element {
+	return <></>;
 }
--- tests/Accordion.spec.tsx
+++ tests/Accordion.spec.tsx
@@ -1,19 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 import { Accordion } from "../src/Accordion";
 
 test("Accordion displays correctoy", () => {
-	render(<Accordion
-		header={<h1>Accordion</h1>}
-		content={<p>
-			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure,
-			laudantium eveniet. Nulla facilis nostrum inventore, aperiam
-			laudantium placeat ? Distinctio, facilis ? Placeat commodi quibusdam
-			maxime illum quo dolorem corporis incidunt asperiores. 1
-		</p>}
-	/>);
-
-	const heading = screen.getByText(/Accordion/i);
-
-	expect(heading).toBeInTheDocument();
+	render(<Accordion />);
 });
