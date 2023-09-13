9ed57ea46f498de1691e231e9cea3b4586c763a2
fponce <fponce@cash-converters.es>
2023-09-13 15:54:48 +0200

    #8: add click test

--- tests/Accordion.cy.tsx
+++ tests/Accordion.cy.tsx
@@ -67,4 +67,30 @@ describe("<Accordion />", () => {
       .should("be.visible")
       ;
   });
+
+  it("onClick", () => {
+    cy.mount(
+      <Accordion
+        header={<h1>Hola Mundo</h1>}
+        content={
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure,
+            laudantium eveniet. Nulla facilis nostrum inventore, aperiam
+            laudantium placeat ? Distinctio, facilis ? Placeat commodi quibusdam
+            maxime illum quo dolorem corporis incidunt asperiores. 4
+          </p>
+        }
+      />
+    );
+
+    cy.get('.accordion-content')
+      .should("have.class", "hidden")
+      .should("not.be.visible")
+      ;
+    cy.get('.accordion-header').click();
+    cy.get('.accordion-content')
+      .should("not.have.class", "hidden")
+      .should("be.visible")
+      ;
+  });
 });
