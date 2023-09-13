b35411f74a82611218bdd2fdc1416c7ca7ae7409
fponce <fponce@cash-converters.es>
2023-09-13 14:18:19 +0200

    \#2: accordion jest test failed
    (without component definition)

--- tests/Accordion.spec.tsx
+++ tests/Accordion.spec.tsx
@@ -1,7 +1,19 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import { Accordion } from "../src/Accordion";
 
-test("Accordion displays correctoy", () => {
-	render(<Accordion />);
+test("Accordion displays correctly", () => {
+	render(<Accordion
+		header={<h1>Accordion</h1>}
+		content={<p>
+			Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure,
+			laudantium eveniet. Nulla facilis nostrum inventore, aperiam
+			laudantium placeat ? Distinctio, facilis ? Placeat commodi quibusdam
+			maxime illum quo dolorem corporis incidunt asperiores. 1
+		</p>}
+	/>);
+
+	const heading = screen.getByText(/Accordion/i);
+
+	expect(heading).toBeInTheDocument();
 });
